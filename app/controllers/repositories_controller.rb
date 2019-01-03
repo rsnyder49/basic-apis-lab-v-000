663e90daf00cc4a7ce5186eeefc46e189ae04cce
Robert Snyder <r.snyder49@gmail.com>
2019-01-03 00:54:22 +0000

    Automatically backed up by Learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -8,16 +8,16 @@ class RepositoriesController < ApplicationController
     @resp = Faraday.get 'https://api.github.com/search/repositories' do |req|
     req.params['client_id'] = 'id'
     req.params['client_secret'] = 'secret'
-       req.params['q'] = params[:query]
-    end
+    req.params['q'] = params[:query]
+  end
  
-    body = JSON.parse(@resp.body)
-    if @resp.success?
-      @repos = body["items"]
-    else
-      @error = body["meta"]["errorDetail"]
-    end
-     render 'search'
-   end
+  body = JSON.parse(@resp.body)
+  if @resp.success?
+    @repos = body["items"]
+  else
+    @error = body["meta"]["errorDetail"]
+  end
+  render 'search'
+  end
    
 end
